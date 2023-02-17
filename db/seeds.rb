d14c261b269d51ebac8cf47251e7d6083570033d
Anna Poltieva <anna.poltieva@kennasecurity.com>
2023-02-17 12:38:19 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+if Lecture.all.empty?
+  Lecture.create([
+                   {id: 1, name: 'English 101'},
+                   {id: 2, name: 'How to start a conversation'},
+                   {id: 3, name: '1000 most common words in English'},
+                   {id: 4, name: 'How to express your feelings'}
+                 ])
+end
